7683c0c534b903be0cf252e3348be68b4e620e28
Matthew Stadelman <stadelmanma@gmail.com>
2020-02-20 06:11:46 -0500

    Implement logic to parse FIT message headers

--- src/parser.rs
+++ src/parser.rs
@@ -10,14 +10,14 @@ use std::fmt::Display;
 use nom::IResult;
 use nom::number::complete::{le_u8, le_u16, le_u32};
 
-// FIT file header format should be 12-14 bytes
-// header_size = u8,
-// protocol_ver_enc = u8,
-// profile_ver_enc = u16
-// data_size = u32
-// literal ".FIT" = [u8; 4]
-// CRC = u16 (if the header_size is 14 bytes)
-#[derive(Clone,Debug)]
+/// FIT file header format should be 12-14 bytes
+/// header_size = u8,
+/// protocol_ver_enc = u8,
+/// profile_ver_enc = u16
+/// data_size = u32
+/// literal ".FIT" = [u8; 4]
+/// CRC = u16 (if the header_size is 14 bytes)
+#[derive(Clone, Debug)]
 pub struct FitFileHeader {
   pub header_size: u8,
   pub protocol_ver_enc: f32,
@@ -25,6 +25,30 @@ pub struct FitFileHeader {
   pub data_size: u32,
   pub crc: Option<u16>
 }
+
+/// Fit messages contain data or a definition of the data in the next message(s)
+#[derive(Clone, Debug)]
+pub enum FitMessageType {
+  Data,
+  Definition
+}
+
+/// /FIT message headers are a single byte long and come in two forms.
+///
+/// The value of the bits inside is different for the two message header types
+#[derive(Clone, Debug)]
+pub enum FitMessageHeader {
+  Normal {
+    message_type: FitMessageType,
+    contains_developer_data: bool,
+    local_message_type: u8
+  },
+  CompressedTimestamp {
+    local_message_type: u8,
+    time_offset: u8
+  }
+}
+
 
 named!(pub header<FitFileHeader>,
   do_parse!(
@@ -45,20 +69,65 @@ named!(pub header<FitFileHeader>,
   )
 );
 
+fn message_header(input: &[u8]) -> IResult<&[u8], FitMessageHeader> {
+  let msg_header_byte: u8 = le_u8(input)?.1;
+
+  if msg_header_byte & 0x80 == 1 {
+    Ok((&input[1..], FitMessageHeader::CompressedTimestamp {
+      local_message_type: (msg_header_byte >> 5) & 0x3, // bits 5-6,
+      time_offset: msg_header_byte & 0x1F
+    }))
+  }
+  else {
+    let msg_type = if msg_header_byte & 0x40 == 1 {
+      FitMessageType::Data
+    }
+    else {
+      FitMessageType::Definition
+    };
+    Ok((&input[1..], FitMessageHeader::Normal {
+      message_type: msg_type,
+      contains_developer_data: msg_header_byte & 0x20 == 1,
+      local_message_type: msg_header_byte & 0xF
+    }))
+  }
+}
+
+
+
 /// Convert a split decimal style value with fix precisions into a single floating point value
 ///
 /// this function should never fail as long as integer values are passed in as the arguments
 fn split_decimal_to_float<T: Display>(left: T, right: T) -> f32 {
-    format!("{}.{}", left, right).parse().unwrap()
+  format!("{}.{}", left, right).parse().unwrap()
 }
 
-#[test]
-fn header_test() {
-  let data = include_bytes!("../test/fixtures/Activity.fit");
-  let hdr = header(data).unwrap().1;
-  assert_eq!(hdr.header_size, 12);
-  assert_eq!(hdr.protocol_ver_enc, 1.0);
-  assert_eq!(hdr.profile_ver_enc, 1.0);
-  assert_eq!(hdr.data_size, 757);
-  assert_eq!(hdr.crc, None);
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  // I could define static byte slices for these basic functions to operate on
+  // without loading an actual fixture file. I'll still want full file tests
+  // down the road
+
+  #[test]
+  fn header_test() {
+    let data = include_bytes!("../test/fixtures/Activity.fit");
+    let hdr = header(data).unwrap().1;
+    assert_eq!(hdr.header_size, 12);
+    assert_eq!(hdr.protocol_ver_enc, 1.0);
+    assert_eq!(hdr.profile_ver_enc, 1.0);
+    assert_eq!(hdr.data_size, 757);
+    assert_eq!(hdr.crc, None);
+  }
+
+  #[test]
+  fn message_header_test() {
+    let data = include_bytes!("../test/fixtures/Activity.fit");
+    let sl = &data[12..];
+    let hdr = message_header(sl).unwrap().1;
+    // need to asert that this is what I'm returning
+    //Normal { message_type: Definition, contains_developer_data: false, local_message_type: 0 }
+    // also need to test a CompressedTimestamp version and a Normal, Data version
+  }
 }
