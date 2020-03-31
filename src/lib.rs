8ad83a45b3bbbdb6d80e54c5e4bdffcbbd9ef7af
Matthew Stadelman <stadelmanma@gmail.com>
2020-03-31 17:23:12 -0400

    Update test code to match new parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -30,85 +30,86 @@ pub fn parse<T: Read>(source: &mut T) -> Result<Vec<FitFile>, Box<dyn std::error
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
 
     #[test]
     fn parse_activity() {
-        let data = include_bytes!("../test/fixtures/Activity.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/Activity.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 22);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     #[ignore]
     fn parse_developer_data() {
-        let data = include_bytes!("../test/fixtures/DeveloperData.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/DeveloperData.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 6);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_monitoring_file() {
-        let data = include_bytes!("../test/fixtures/MonitoringFile.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/MonitoringFile.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 355);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_settings() {
-        let data = include_bytes!("../test/fixtures/Settings.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/Settings.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 3);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_weight_scale_multi_user() {
-        let data = include_bytes!("../test/fixtures/WeightScaleMultiUser.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WeightScaleMultiUser.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 7);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_weight_scale_single_user() {
-        let data = include_bytes!("../test/fixtures/WeightScaleSingleUser.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WeightScaleSingleUser.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 6);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_workout_custom_target_values() {
-        let data = include_bytes!("../test/fixtures/WorkoutCustomTargetValues.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WorkoutCustomTargetValues.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 6);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_workout_individual_steps() {
-        let data = include_bytes!("../test/fixtures/WorkoutIndividualSteps.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WorkoutIndividualSteps.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 6);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_workout_repeat_greater_than_step() {
-        let data = include_bytes!("../test/fixtures/WorkoutRepeatGreaterThanStep.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WorkoutRepeatGreaterThanStep.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 7);
-        assert_eq!(r.len(), 0);
     }
 
     #[test]
     fn parse_workout_repeat_steps() {
-        let data = include_bytes!("../test/fixtures/WorkoutRepeatSteps.fit");
-        let (r, fitfile) = parse_file(data).unwrap();
+        let data = include_bytes!("../test/fixtures/WorkoutRepeatSteps.fit").to_vec();
+        let mut buf = Cursor::new(&data);
+        let fitfile = &parse(&mut buf).unwrap()[0];
         assert_eq!(fitfile.records.len(), 7);
-        assert_eq!(r.len(), 0);
     }
 }
