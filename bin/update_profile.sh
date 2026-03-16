6006f0f47d28039380cbbda9a022ddc65bce7681
Richard Young <code@richyoung.ca>
2026-03-15 22:59:15 -0400

    feat: allow manual FIT SDK version override

--- bin/update_profile.sh
+++ bin/update_profile.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-exec cargo run --bin generate-fit-profile -- "$@"
+
+set -eu
+
+profile_path="${1:-}"
+sdk_version="${2:-}"
+
+if [ -z "$profile_path" ]; then
+    echo "Usage: $0 PROFILE.xlsx [SDK_VERSION]" >&2
+    exit 1
+fi
+
+if [ -n "$sdk_version" ]; then
+    exec cargo run --bin generate-fit-profile -- "$profile_path" --sdk-version "$sdk_version"
+fi
+
+exec cargo run --bin generate-fit-profile -- "$profile_path"
